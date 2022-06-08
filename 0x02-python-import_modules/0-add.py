feb5e906662645202623fce57cd64c76450538ee
Alain Bauer <73667214+Bauer123@users.noreply.github.com>
2022-06-08 22:53:42 +0300

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,12 @@
 #!/usr/bin/python3
-import sys
+def add(a, b):
+    """My addition function
 
+    Args:
+        a: first integer
+        b: second integer
 
-def main():
-    argc = len(sys.argv) - 1
-    if argc == 0:
-        print("0 arguments.")
-    elif argc == 1:
-        print("1 argument:")
-        print("1: {}".format(sys.argv[1]))
-    else:
-        print("{:d} arguments:".format(argc))
-        for i in range(1, argc + 1):
-            print("{}: {}".format(i, sys.argv[i]))
-
-if __name__ == "__main__":
-    main()
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
